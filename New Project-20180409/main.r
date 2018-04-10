7f7176ac851ea60adde22af230700f8cce1673c4
AnaPaulaS <anapaulac@live.com>
2018-04-10 10:02:48 -0300

    Created by Coding Ground Client

---
+++ New Project-20180409/main.r
@@ -0,0 +1,10 @@
+t2 = read.csv2("bla.csv", col.names = c("primeiro", "segundo", "terceiro", "quarto"))
+#t2$primeiro
+
+t3 = read.csv2("planejamento_ppag_2017.csv", col.names = c("uo","prog","acao","subacao","descricao","descricao_prod","qtde_prevista_orig","qtde_concedida","ind_meta_cumulativa","qtde_exec_ate_per","qtde_exec_orig","qtde_exec_red","qtde_exec_total","eficacia_final","valor_orcado","valor_emp_orig","valor_liquid","eficiencia_liq_final","sequencia_sub_acao"))
+t3$eficiencia_liq_final
+
+
+
+#, col.names = c("uo","prog","acao","subacao","descricao","descricao_prod","qtde_prevista","qtde_concedida","ind_meta_cumulativa","qtde_exec_ate_per","qtde_exec_orig","qtde_exec_red","qtde_exec_total","eficacia_final","valor_orcado","valor_emp_orig","valor_liquid,"eficiencia_liq_final","sequencia_sub_acao"")
+
